e7a1b0298962946101d5af7b93161de509af9f46
Jhana Barnes <jhana@Jamels-MacBook-Pro.local>
2021-09-28 20:25:54 -0500

    This is my first commit.

--- src/Hint.java
+++ src/Hint.java
@@ -7,7 +7,62 @@ import java.lang.*;
 
 public class Hint {
 
+    private String hintText;
+    private String correctAnswer;
 
+
+
+    // Constructor for Hint Object which contains the hint and the answer
+    public Hint(String hintText, String correctAnswer) throws FileNotFoundException {
+        this.hintText = hintText;
+        this.correctAnswer = correctAnswer;
+    }
+
+
+    // Getters
+    public String getHintText(){
+        return hintText;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+
+
+
+
+
+
+
+/*
+
+    public boolean isCorrect() {
+        return playerAnswer == correctAnswer;
+    }
+
+    public String getHintText() {
+        return hintText;
+    }
+
+    public String getAnswer0() {
+        return answer0;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+*/
+
+/*
     public void DisplayHint() throws IOException {
         int min = 0;
         int max = 10;
@@ -28,51 +83,12 @@ public class Hint {
         }
         System.out.println("No More Questions!");
     }
+   */
+
 }
 
 
 
-
-
-
-
-
-
-/*
-    public int randomIndex(){
-        int min = 0;
-        int max = 9;
-        Random randomInt = new Random();
-        int value = randomInt.nextInt(max + min) + min;
-        return value;
-
-    }
-
-
-    public void DisplayHint() throws IOException {
-        int n = (listIndex()); // The line number
-        String hintLine;
-
-            try (BufferedReader br = new BufferedReader(new FileReader("/Users/jhana/IdeaProjects/Chatwah/src/Hints.txt"))) {
-
-                for (int i = 0; i < max; i++) {
-                    br.readLine();
-                    hintLine = br.readLine();
-                    while (hintLine != null) {
-                        System.out.println(hintLine);
-                        System.out.println("Press \"ENTER\" to continue...");
-                        Scanner scanner = new Scanner(System.in);
-                        scanner.nextLine();
-                        list.remove(0);
-                    }
-                }
-
-            } catch (IOException e) {
-                System.out.println(e);
-            }
-        }
-
- */
 
 
 
