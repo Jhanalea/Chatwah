56cbc063b6a266e5962f58bee8f4a9998253f7b9
Jhana Barnes <jhanabarnes@gmail.com>
2021-12-28 17:11:24 -0500

    Cleaned Up Unnecessary Code

--- src/Hint.java
+++ src/Hint.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.nio.file.*;
-import java.util.*;
 import java.lang.*;
 
 
@@ -18,7 +16,6 @@ public class Hint {
         this.correctAnswer = correctAnswer;
     }
 
-
     // Getters
     public String getHintText(){
         return hintText;
@@ -28,62 +25,6 @@ public class Hint {
         return correctAnswer;
     }
 
-
-
-
-
-
-
-
-/*
-
-    public boolean isCorrect() {
-        return playerAnswer == correctAnswer;
-    }
-
-    public String getHintText() {
-        return hintText;
-    }
-
-    public String getAnswer0() {
-        return answer0;
-    }
-
-    public String getAnswer1() {
-        return answer1;
-    }
-
-    public String getAnswer2() {
-        return answer2;
-    }
-
-    public String getAnswer3() {
-        return answer3;
-    }
-*/
-
-/*
-    public void DisplayHint() throws IOException {
-        int min = 0;
-        int max = 10;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = min; i < max; i++) {
-            list.add(i);
-        }
-        Collections.shuffle(list);
-
-        while (list.size() > min) {
-            String hintLine = Files.readAllLines(Paths.get("/Users/jhana/IdeaProjects/Chatwah/src/Hints.txt")).get(list.get(0));
-            System.out.println(hintLine);
-            list.remove(0);
-            System.out.println("Press \"ENTER\" to continue...");
-            Scanner scanner = new Scanner(System.in);
-            scanner.nextLine();
-
-        }
-        System.out.println("No More Questions!");
-    }
-   */
 
 }
 
